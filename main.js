cb8b29095e1702b1c29820eec849791624a445a2
yahshemi <yahshemi@gmail.com>
2022-12-19 23:23:49 -0800

    adds styling

--- main.js
+++ main.js
@@ -1,30 +1,24 @@
 function handleClickEvent() {
+  var amountDue = parseFloat( $("#amount-due").val());
+  var amountReceived = parseFloat($("#amount-received").val());
+  console.log(amountReceived, amountDue)
 
-  var amountDueElm = $("#amount-due");
-  var amountReceivedElm = $("#amount-received");
-  var btnElm = $("#calculate-change");
-// Convert the string into a number
-  var amountDue = parseFloat(amountDueElm.val());
-  var amountReceived = parseFloat(amountReceivedElm.val());
-  // Get the difference and convert into pennies
   var runningTotal = (amountReceived - amountDue) * 100;
-  $('#your-change-is').html('$' + runningTotal / 100);
-  
-  // Create individual JSON objects for the denoms dollar - pennies
+  $("#your-change-is").html("$" + runningTotal / 100);
+
   var denomList = [
-    { "where": '#dollars-output',  "value": 100 }, 
-    { "where": '#quarters-output', "value": 25 }, 
-    { "where": '#dimes-output',    "value": 10 },
-    { "where": '#nickels-output',  "value": 5 }, 
-    { "where": '#pennies-output' }
+    { where: "#dollars-output", value: 100 },
+    { where: "#quarters-output", value: 25 },
+    { where: "#dimes-output", value: 10 },
+    { where: "#nickels-output", value: 5 },
+    { where: "#pennies-output" },
   ];
-  // loop through denomList
-  denomList.forEach(function(item){
+  
+  denomList.forEach(function (item) {
     if (item.value) {
       $(item.where).html(parseInt(runningTotal / item.value));
       runningTotal %= item.value;
     } else {
-      // console.log(runningTotal);
       $(item.where).html(Math.round(runningTotal));
     }
   });
